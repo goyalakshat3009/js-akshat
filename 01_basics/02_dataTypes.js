34d76c297a0e6f0e522b642380dc4df57ccde302
goyalakshat3009 <akshatgoyal795@gmail.com>
2026-07-06 09:43:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(6+9) // we are using nodejs, not browser this is true in browser not nodejs
+
+console.log(3+3);
+console.log("Akshat");
+
+let name="Akshat"
+let age=18
+let temp=false
+
+// number ==> 2 to power 53
+//  BigInt
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> 
+// symbol ==> unique
+
+// object
+
+console.log(typeof "Ram");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
