0750da38f286b55c66eef774e2146c5c0323ee28
goyalakshat3009 <akshatgoyal795@gmail.com>
2026-07-06 10:19:09 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,43 @@ let bool = Boolean(is)
 
 let someNumber=33
 let string = String(someNumber)
-console.log(string)
-console.log(typeof string)
+// console.log(string)
+// console.log(typeof string)
+
+// ********* Operations **********
+
+let val=3
+let negValue = -val
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**6);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Akshat"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+ 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(true+); error invalid syntax
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let Counter=100
+Counter++;
+console.log(Counter)
