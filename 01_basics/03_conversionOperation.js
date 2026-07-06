dc080cf82474eccd603ff5f897630b07d08235c8
goyalakshat3009 <akshatgoyal795@gmail.com>
2026-07-06 10:02:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="22abc"
+let valueInNumber=Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let temp=null
+let value=Number(temp)
+// console.log(typeof temp)
+// console.log(typeof value)
+// console.log(temp)
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true=>1; false=>0
+
+let is="Akshat"
+let bool = Boolean(is)
+// console.log(bool)
+
+// 1 ==> true; 0==> false
+// ""=>false
+// "akshat" ==> true
+
+let someNumber=33
+let string = String(someNumber)
+console.log(string)
+console.log(typeof string)
