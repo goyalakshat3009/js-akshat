7f503936dce4e5dafbe88d8a209756c382efa4a2
goyalakshat3009 <akshatgoyal795@gmail.com>
2026-07-06 04:20:44 +0000

    commit config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID=144889
+let accountEmail = "akshatgoyal795@gmail.com"
+var accountPassword="12345678"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID=2 not allowed
+
+accountEmail = "heoio0o@gmail.com"
+accountPassword="1768727"
+accountCity="Bangaluru"
+
+console.log(accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+Prefer not to use var
+because of issue in blovk scope and functional scope
+*/
